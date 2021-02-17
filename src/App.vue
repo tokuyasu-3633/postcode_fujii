1a4787e62b6d71a2864ecc3d3911bb9e1d099d10
tokuyasu <fujiiakito3633@gmail.com>
2021-02-18 01:21:25 +0900

    second

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
     <input type="text" v-model="number">
-    <p>{{number}}</p>
-    <p>{{allAdress}}</p>
     <button @click="push">住所自動入力</button>
     <p>Address：{{name}}</p>
   </div>
@@ -13,18 +11,15 @@ export default {
   data(){
     return{
       number: "",
-      allAdress: "",
       name: ""
     }
   },
-  async created() {
-    const item = await axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/${this.number}?apiKey=HkxyMitjcUp3biK3xWHQunXt1qKfqjbBXdIyuhf`);
-    const data = item.prefCode;
-    this.allAdress = data;
-  },
   methods: {
     push() {
-      this.name = this.allAdress;
+      axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/${this.number}?apiKey=HkxyMitjcUp3biK3xWHQunXt1qKfqjbBXdIyuhf`).then(response => {
+        console.log(response.data[0].allAddress);
+        this.name = response.data[0].allAddress;
+      })
     }
   }
 }
